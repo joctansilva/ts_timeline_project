58e4a7ae69ddfd56dced05251b5a1a9a356ffcd0
Joctan Neves <joctan.silva@trinusco.com.br>
2023-12-17 18:13:19 -0300

    feat: home page mobile created, and project configuration

--- mobile/prettier.config.js
+++ mobile/prettier.config.js
@@ -1,3 +1,15 @@
-module.export = {
-  plufins: [ require('prettier-plugin-tailwindcss')]
-}
+module.exports = {
+  arrowParens: "always",
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "auto",
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+};
