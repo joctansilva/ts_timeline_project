5e43e0faa24a351ad397d91f440545cbb009c421
Joctan Neves <joctan.silva@trinusco.com.br>
2023-12-17 13:21:23 -0300

    feat: create home page on frontend, and configure prettier

--- web/prettier.config.js
+++ web/prettier.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-}
+  arrowParens: "always",
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "auto",
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+};
