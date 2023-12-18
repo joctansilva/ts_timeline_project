97ed9049cda860270c900a47488e74a42b759cbd
Joctan Neves <joctan.silva@trinusco.com.br>
2023-12-17 23:38:48 -0300

    feat: create conection to github and separate components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,22 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💖 pelo{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/joctansilva"
+        className="underline hover:text-gray-100"
+      >
+        Joctan Silva
+      </a>{" "}
+      no NLW da{" "}
+      <a
+        href="https://www.rocketseat.com.br"
+        className="underline hover:text-gray-100"
+      >
+        Rocketset{" "}
+      </a>
+    </div>
+  );
+}
