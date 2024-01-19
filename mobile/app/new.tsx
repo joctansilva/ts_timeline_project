286cd3dbc64ffbd34ebe4f83d4decf71011a9325
Joctan Neves <joctan.silva@trinusco.com.br>
2024-01-19 11:48:04 -0300

    finished config mobile

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,71 +1,109 @@
-import { ScrollView, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
-import Icon from "@expo/vector-icons/Feather";
-import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
-import { Link } from "expo-router";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { useState } from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+import * as SecureStore from 'expo-secure-store'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link, useRouter } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+import { api } from '../src/lib/api'
+
+dayjs.locale(ptBR)
+
+interface Memory {
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+  id: string
+}
 
 export default function NewMemory() {
-  const { bottom, top } = useSafeAreaInsets();
+  const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+  const [memories, setMemories] = useState<Memory[]>([])
 
-  const [isPublic, setIsPublic] = useState(false);
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
 
   return (
     <ScrollView
-      className="flex-1 px-8"
+      className="flex-1"
       contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
-      <View className="flex-row mt-4 items-center justify-between">
+      <View className="mt-4 flex-row items-center justify-between px-8">
         <NLWLogo />
-        <Link href="/memories" asChild>
-          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
-            <Icon name="arrow-left" size={16} color="#fff" />
-          </TouchableOpacity>
-        </Link>
-      </View>
-      <View className="mt-6 space-y-6">
-        <View className="flex-row items-center gap-2">
-          <Switch
-            value={isPublic}
-            onValueChange={setIsPublic}
-            thumbColor={isPublic ? "#9b79ea" : "#9e9ea0"}
-            trackColor={{ false: "#767577", true: "#372560" }}
-          />
-          <Text
-            className="font-body text-base txt-gray-200"
-            style={{ color: "#9e9ea0" }}
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
           >
-            Tornar Memória Pública
-          </Text>
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
         </View>
-        <TouchableOpacity className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" size={24} color="#fff" />
-            <Text
-              className="font-body text-sm txt-gray-200"
-              style={{ color: "#9e9ea0" }}
-            >
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
-        </TouchableOpacity>
+      </View>
 
-        <TextInput
-          multiline
-          className="p-0 font-body text-lg text-gray-50"
-          placeholderTextColor="#56565a"
-          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência"
-        />
-        <TouchableOpacity
-          activeOpacity={0.7}
-          className="items-center rounded-full bg-green-500 px-5 py-3"
-
-        >
-          <Text className="font-alt text-sm uppercase text-black">
-            Salvar
-          </Text>
-        </TouchableOpacity>
+      <View className="mt-6 space-y-10">
+        {memories.map((memory) => {
+          return (
+            <View key={memory.id} className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50" />
+                <Text className="font-body text-sm text-gray-100">
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+                </Text>
+              </View>
+              <View className="space-y-4 px-8">
+                <Image
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                  className="aspect-video w-full rounded-lg"
+                  alt=""
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="font-body text-sm text-gray-200">
+                      Ler mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
-  );
+  )
 }
